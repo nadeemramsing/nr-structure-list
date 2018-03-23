e7e6737c1d6d63cb7f5fe28c452a5eb482358ea7
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-03-23 16:08:46 +0400

    "documents" two-way binding used to display in component.

--- src/nr-structure-list.js
+++ src/nr-structure-list.js
@@ -6,7 +6,7 @@
         .module('NrStructureList', ['NrAngularPagination'])
         .component('nrStructureList', {
             bindings: {
-
+                documents: '='
             },
             templateUrl: 'tpl/nr-structure-list.html',
             controller: StructureListController,
@@ -14,8 +14,11 @@
         });
 
     function StructureListController($scope, $http, $httpParamSerializer, $q) {
+        var vm = this;
 
+        this.$onInit = function () {
 
+        }
     }
 
 })();
